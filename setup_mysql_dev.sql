f03f3ccdfe3544607808ca88c1ecbe8e32007bdf
kwenziwa <kwenziwa@live.com>
2023-08-22 09:40:06 -0700

    bug fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
+-- MySQL Script that prepares the server for this project
+
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 -- Create the user if it doesn't exist and set the password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grant all privileges on hbnb_dev_db to hbnb_dev
