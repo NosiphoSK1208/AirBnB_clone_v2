64e25412aaeb00e4040666dca5c152f990932727
kwenziwa <kwenziwa@live.com>
2023-08-22 10:07:12 -0700

    buf fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,12 @@
+
+
 -- MySQL Script that prepares the server for this project
 
 -- Create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create the user if it doesn't exist and set the password
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- Grant all privileges on hbnb_dev_db to hbnb_dev
-GRANT ALL on hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
--- Grant SELECT privilege on performance_schema to hbnb_dev
-GRANT SELECT on performance_schema.* TO 'hbnb_dev'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Grant all privileges on hbnb_test_db to hbnb_test
+GRANT ALL on hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Grant SELECT privilege on performance_schema to hbnb_test
+GRANT SELECT on performance_schema.* TO 'hbnb_test'@'localhost';
