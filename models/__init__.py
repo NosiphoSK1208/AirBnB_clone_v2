f344647d9db42494d63a5e7d0bc14473a9357543
NosiphoSK1208 <Simphiwe95.k@gmail.com>
2023-08-22 18:05:43 -0700

    model update

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
+from os import environ
+from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
+if environ['HBNB_TYPE_STORAGE'] == 'db':
+    # Use DBStorage as the storage method
+    storage = DBStorage()
+else:
+    # Use FileStorage as the storage method
+    storage = FileStorage()
 
-storage = FileStorage()
+# Load data into the storage
 storage.reload()
